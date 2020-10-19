19908173d0b306c23794fc4b92cf4423f83c7afc
matt_kuras <mattkuras@gmail.com>
2020-10-19 14:43:57 -0400

    add and style header

---
+++ app/javascript/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => (
+    <div className='header'>
+        <h1>End the War on Drugs</h1>
+    </div>
+)
+
+export default Header
