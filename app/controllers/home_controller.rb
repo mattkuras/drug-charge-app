bb45522e2ff50572e3eb8bba6cd7493aa315a2fa
matt_kuras <mattkuras@gmail.com>
2020-10-19 10:37:53 -0400

    fix conditional render of method in create action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,23 @@ class HomeController < ApplicationController
 
     def create
         drug = params[:drug].downcase
-        puts drug
         state = params[:state].downcase.split(' ').join('-')
-        charge = params[:charge].downcase
+        charge = params[:charge].downcase 
+        puts charge 
         table = Scraper.run(state, drug)
-        render json: table.possession 
+        if charge == 'possession'
+            charge = table.possession
+            elsif charge == 'sale'
+                charge = table.sale
+                elsif charge == 'trafficking'
+                    charge = table.trafficking
+                end
+        # elsif charge == 'Sale'
+        #         render json: table.sale
+        # elsif charge =='Trafficking'
+        #         render json: table.trafficking
+        # end
+        render json: charge
     end
     
 end
